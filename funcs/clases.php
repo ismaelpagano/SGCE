6cb28dd623b988df43905119d0a40f9def0a102f
Ismael Pagano <ismaelpagano@gmail.com>
2023-03-11 21:14:12 -0300

    Quitado impresión de marca fecha_rof

--- funcs/clases.php
+++ funcs/clases.php
@@ -971,8 +971,6 @@
            
             $div_botones = '';
             $div_rof = '';
-
-            echo $fecha_lim_rof;
     
             if ( ($estado_compra == 4 || $estado_compra == 5) && $fecha_lim_rof > time() ) {
                 $div_botones = 
